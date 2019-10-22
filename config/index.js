3d89998ead8ea66d6571d2ab8a7175cffc9d6277
Jamie Dawson <jadawson@e1z3r5p1.42.us.org>
2019-10-21 21:54:44 -0700

    fixed mongodb issue

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  mongo_uri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/instaclone',
+  mongo_uri:
+    'mongodb+srv://jamie123:jamie123@instaclone-fxfsx.mongodb.net/test?retryWrites=true&w=majority',
   secret: process.env.SECRET || 'CodingIsCool'
 };
